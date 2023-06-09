b4cfb48fc0e6ea3af2a9e90c10bd762110f44540
re1983 <re1983@msn.com>
2023-06-09 10:56:11 -0600

    LAB PC

--- ImageToVedio.py
+++ ImageToVedio.py
@@ -4,14 +4,14 @@ import glob
 
 flag = False
 
-for filename in sorted(glob.glob('/home/jj/Documents/AirSim/2023-06-08-16-28-15/images/*.ppm')):
+for filename in sorted(glob.glob('/home/jj/Documents/AirSim/2023-06-09-09-57-52/images/*.ppm')):
     print(filename)
     img = cv2.imread(filename)
     if not flag:
         height, width, layers = img.shape
         frameSize = (width,height)
-        out = cv2.VideoWriter('/home/jj/Documents/AirSim/2023-06-08-16-28-15/output_video_15.mp4',
-                                cv2.VideoWriter_fourcc(*'mp4v'), 15, frameSize)
+        out = cv2.VideoWriter('/home/jj/Documents/AirSim/2023-06-09-09-57-52/output_video_30.mp4',
+                                cv2.VideoWriter_fourcc(*'mp4v'), 30, frameSize)
         flag = True
         # continue
     out.write(img)
